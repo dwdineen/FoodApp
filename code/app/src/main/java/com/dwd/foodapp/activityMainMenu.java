2328cf0bf99adff1ab77910d051435fc9e7d59fa
Daniel Dineen <Daniel Dineen>
2016-05-28 19:35:45 -0400

    Started Project

---
+++ code/app/src/main/java/com/dwd/foodapp/activityMainMenu.java
@@ -0,0 +1,13 @@
+package com.dwd.foodapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activityMainMenu extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_activity_main_menu);
+	}
+}
