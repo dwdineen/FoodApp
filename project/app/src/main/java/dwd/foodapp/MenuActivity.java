c16714f5afdbbeae3bb2e5ac7eb568c7ad1fded6
Daniel Dineen <Daniel Dineen>
2016-05-29 10:56:13 -0400

    Stuff

--- project/app/src/main/java/dwd/foodapp/MenuActivity.java
+++ project/app/src/main/java/dwd/foodapp/MenuActivity.java
@@ -1,7 +1,10 @@
 package dwd.foodapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+		Button InventoryButton = (Button)findViewById(R.id.btnInventory);
+		InventoryButton.setOnClickListener(A);
+
+
     }
+
+
+	View.OnClickListener A = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Button btn = (Button)v;
+			btn.setText("Test");
+		}
+	};
+
+
+
 }
